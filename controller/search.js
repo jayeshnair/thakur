4c795e1c63b962434e0b21aeb1a16009aca5eb88
Jayesh Nair <jayesh.nair@olx.com>
2018-01-30 14:55:21 +0530

    console

--- controller/search.js
+++ controller/search.js
@@ -2,6 +2,7 @@ const helper = require('./../helper')
 
 getSearchResults = (req, res) => {
     const data = req.body;
+    console.log(data);
     const searchResults = helper.searchResults(data, (error, body) => {
         res.send(body)
     });
