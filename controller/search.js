a54d5c8ccf378aaeb39ecdfd60aedbb0c0ebab47
Jayesh Nair <jayesh.nair@olx.com>
2018-01-31 12:30:16 +0530

    test

--- controller/search.js
+++ controller/search.js
@@ -4,10 +4,6 @@ getSearchResults = (req, res) => {
     const data = req.body;
     const searchResults = helper.searchResults(data, (error, body) => {
         let apiResponce = helper.apiResponseTemplateDef('Showing results', body);
-        if (Object.keys(body).length) {
-            apiResponce = helper.apiResponseTemplate('Showing results', body);
-        }
-        
         res.send(apiResponce);
     });
 }
