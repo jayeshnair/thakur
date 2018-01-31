8ac1ac6888c10d2ee6568c2eb2fe122c62aa5e4f
Jayesh Nair <jayesh.nair@olx.com>
2018-01-31 16:31:07 +0530

    fix

--- controller/search.js
+++ controller/search.js
@@ -4,11 +4,14 @@ getSearchResults = (req, res) => {
     const data = req.body;
     const searchResults = helper.searchResults(data, (error, apiData) => {
         console.log(apiData);
+        console.log(error);
         console.log("##########################################################################");
-        let apiResponce = helper.apiResponseTemplate('Showing results', apiData);
-        console.log(apiResponce);
-        res.json(apiResponce);
-        console.log("##########################################################################");
+        // if(typeof apiData === 'undefined') {
+            let apiResponce = helper.apiResponseTemplate('Showing results', apiData);
+            console.log(apiResponce);
+            res.json(apiResponce);
+            console.log("##########################################################################");
+        // }
     });
 }
 
